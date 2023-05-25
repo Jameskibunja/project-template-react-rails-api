089bb2c2a5730c781edc3f0ccbfc1584c51b70b5
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-05-25 08:00:10 +0300

    added authorize method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,9 +29,4 @@ class ApplicationController < ActionController::API
     render json: { message:"You have to log in"}, status: :unauthorized unless authorized_user
   end
 
-  private
-
-  def set_current_user
-    @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
-  end
 end
