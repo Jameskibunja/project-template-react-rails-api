b94ab434506570d324e512a9f730ef10fdfce218
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-05-25 06:06:23 +0300

    created action to register a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+    def create 
+        user = User.create(user_params)
+        if user.valid
+            token = encode_token({user_id: user.id})
+            render json: {user: user,token: token}, status: :ok
+        else
+            render json: {error: "Invalid username or password"}, status: :uprocessable_entity
+        end
+
+    end
+
+    private
+    def user_params
+        params.require(;users).permit(:name, :password)
+    end
 end
