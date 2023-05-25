3ec9a4207ad5431a26f76509eca8f22c53644d91
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-05-25 10:59:56 +0300

    authorized each action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
     def create 
-        user = User.create(user_params)
-        if user.valid?
-            token = encode_token({user_id: user.id})
-            render json: {user: user, token: token}, status: :ok
+        @user = User.create(user_params)
+        if @user.valid?
+            token = encode_token({user_id: @user.id})
+            render json: {user: @user, token: token}, status: :ok
         else
             render json: {error: "Invalid username or password"}, status: :uprocessable_entity
         end
@@ -11,12 +11,12 @@ class UsersController < ApplicationController
     end
 
     def login
-        user = User.find_by(name: user_params[:name])
-        if user&.authenticate(user_params[:password])
-            token = encode_token({user_id: user.id})
-            render json: {user: user, token: token}, status: :ok
+        @user = User.find_by(name: user_params[:name])
+        if @user&.authenticate(user_params[:password])
+            token = encode_token({user_id: @user.id})
+            render json: {user: @user, token: token}, status: :ok
             else
-            render json: {error: "Invalid username or password"}, status: :uprocessable_entity
+            render json: {error: "Invalid username or password"}, status: :unprocessable_entity
         end
     end
 
