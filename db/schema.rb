b3814b03e934a278acd35a1dad8f958e599640b7
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-05-26 08:48:41 +0300

    merged

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2023_05_25_051231) do
-ActiveRecord::Schema.define(version: 2023_05_25_054119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,16 +27,54 @@ ActiveRecord::Schema.define(version: 2023_05_25_054119) do
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
-  create_table "profiles", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.text "bio"
-    t.string "image"
-    t.string "contacts"
-    t.string "address"
-    t.integer "user_id"
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "book_id", null: false
+    t.decimal "amount"
+    t.string "transaction_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_transactions_on_book_id"
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+  end
+
+  add_foreign_key "books", "users"
+  add_foreign_key "transactions", "books"
+  add_foreign_key "transactions", "users"
+end
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2023_05_25_054119) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
+    t.decimal "price"
+    t.string "image_url"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -68,10 +105,8 @@ ActiveRecord::Schema.define(version: 2023_05_25_054119) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
   end
 
-  add_foreign_key "books", "users"
   add_foreign_key "transactions", "books"
   add_foreign_key "transactions", "users"
 end
