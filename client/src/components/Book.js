9524b1867fe049787a3ec7482e167115ec42559d
JAMES <jameskibunja17@gmail.com>
2023-05-27 09:40:21 +0300

    Upload Feature errors

--- client/src/components/Book.js
+++ client/src/components/Book.js
@@ -3,13 +3,12 @@ import '../styles/Books.css';
 
 const Book = ({ book, onPurchase }) => {
   const { title, author, image_url, description, price } = book;
-  const imagePath = require(`../assets/images/${image_url}`).default;
 
   return (
     <div className="book">
       <h2>{title}</h2>
       <p>{author}</p>
-      <img src={imagePath} alt={title} className="book-image" />
+      {image_url && <img src={image_url} alt={title} className="book-image" />}
       <p>{description}</p>
       <p>{price}</p>
       <button onClick={onPurchase}>Buy</button>
