9843a1c59c902786a5c3826c3ff4e326a141898d
JAMES <jameskibunja17@gmail.com>
2023-05-30 10:55:35 +0300

    Final Commit

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,7 @@ Rails.application.routes.draw do
 
   post '/login', to: 'users#login'
   resources :users, only: [:create]
+
+  # Fallback route
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
